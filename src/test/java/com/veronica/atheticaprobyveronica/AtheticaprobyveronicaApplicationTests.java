8ed59c2a8398097d4e0590e7323c9060c4d53b67
VeronicaCastroMurillo <veronicacastromurillo@gmail.com>
2025-04-29 20:17:32 -0600

    first commit

---
+++ src/test/java/com/veronica/atheticaprobyveronica/AtheticaprobyveronicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veronica.atheticaprobyveronica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtheticaprobyveronicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
